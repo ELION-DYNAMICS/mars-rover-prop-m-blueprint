6a3f70d22f93c4c973b97f735d30b80d0d87f2c9
Rene Grempel <grempelrene@gmail.com>
2026-02-23 12:31:12 +0300

    Create setup.py

---
+++ ros_ws/src/rover_bringup/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+from glob import glob
+import os
+
+package_name = "rover_bringup"
+
+setup(
+    name=package_name,
+    version="0.1.0",
+    packages=[package_name],
+    data_files=[
+        ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
+        ("share/" + package_name, ["package.xml"]),
+        (os.path.join("share", package_name, "launch"), glob("launch/*.launch.py")),
+        (os.path.join("share", package_name, "params"), glob("params/*.yaml")),
+        (os.path.join("share", package_name, "params", "modes"), glob("params/modes/*.yaml")),
+        (os.path.join("share", package_name, "params", "nav2"), glob("params/nav2/*.yaml")),
+    ],
+    install_requires=["setuptools"],
+    zip_safe=True,
+    maintainer="Maintainer",
+    maintainer_email="unknown@example.com",
+    description="Bringup and mode-switch launch for Mars Rover Prop-M Blueprint.",
+    license="Apache-2.0",
+)
