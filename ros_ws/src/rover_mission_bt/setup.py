88ec2adc075f70da03467421c6606071bf84b0dd
Rene Grempel <grempelrene@gmail.com>
2026-02-25 15:06:25 +0300

    Create setup.py

---
+++ ros_ws/src/rover_mission_bt/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+from glob import glob
+import os
+
+package_name = "rover_mission_bt"
+
+setup(
+    name=package_name,
+    version="0.1.0",
+    packages=[package_name],
+    data_files=[
+        ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
+        ("share/" + package_name, ["package.xml"]),
+        (os.path.join("share", package_name, "trees"), glob("trees/*.xml")),
+        ("share/" + package_name, ["README.md"]),
+    ],
+    install_requires=["setuptools"],
+    zip_safe=True,
+    maintainer="Maintainer",
+    maintainer_email="unknown@example.com",
+    description="Behavior Trees for rover mission cycles (Drive -> Stop/Measure -> Transmit/Log).",
+    license="Apache-2.0",
+    entry_points={
+        "console_scripts": [
+            "rover_mission_bt_node = rover_mission_bt.mission_bt_node:main",
+        ],
+    },
+)
