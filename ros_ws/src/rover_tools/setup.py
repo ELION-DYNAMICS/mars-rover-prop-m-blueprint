3bcc782a4ac034267ee702c4be6e0553c47d6af4
Rene Grempel <grempelrene@gmail.com>
2026-02-26 20:45:30 +0300

    setup.py erstellen

---
+++ ros_ws/src/rover_tools/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+from glob import glob
+import os
+
+package_name = "rover_tools"
+
+setup(
+    name=package_name,
+    version="0.1.0",
+    packages=[package_name],
+    data_files=[
+        ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
+        ("share/" + package_name, ["package.xml"]),
+        (os.path.join("share", package_name, "config"), glob("config/*.yaml")),
+        ("share/" + package_name, ["README.md"]),
+    ],
+    install_requires=["setuptools"],
+    zip_safe=True,
+    maintainer="Maintainer",
+    maintainer_email="unknown@example.com",
+    description="Operations tooling: bag recording/export, metrics evaluation, dataset packaging.",
+    license="Apache-2.0",
+    entry_points={
+        "console_scripts": [
+            "rover_tools = rover_tools.cli:main",
+        ],
+    },
+)
